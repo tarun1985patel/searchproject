c8a9b666befdd16031010a4c17f99f856b90ab5d
Tarun Patel <tarun1985patel@gmail.com>
2013-09-18 19:56:20 +0530

    Commit for Search Extension

---
+++ app/code/local/MageWorx/SearchAutocomplete/Model/Observer.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * MageWorx
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MageWorx EULA that is bundled with
+ * this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.mageworx.com/LICENSE-1.0.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the extension
+ * to newer versions in the future. If you wish to customize the extension
+ * for your needs please refer to http://www.mageworx.com/ for more information
+ *
+ * @category   MageWorx
+ * @package    MageWorx_SearchAutocomplete
+ * @copyright  Copyright (c) 2011 MageWorx (http://www.mageworx.com/)
+ * @license    http://www.mageworx.com/LICENSE-1.0.html
+ */
+
+/**
+ * Search Autocomplete extension
+ *
+ * @category   MageWorx
+ * @package    MageWorx_SearchAutocomplete
+ * @author     MageWorx Dev Team
+ */
+
+class MageWorx_SearchAutocomplete_Model_Observer {
+
+    public function regenerateCmsPageIndex($observer) {
+        $page = $observer->getEvent()->getObject();
+
+        Mage::getModel('searchautocomplete/fulltext')->
+                regenerateIndex(null, $page->getId())->
+                resetSearchResults();
+
+        return $this;
+    }
+
+    public function addCategoryToSearchIndex($observer) {        
+        $category = $observer->getEvent()->getCategory();
+        if (!$category || !$category->getId()) return false;
+        
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $tablePrefix = (string) Mage::getConfig()->getTablePrefix();
+
+        $connection->delete($tablePrefix . 'catalogsearch_category_fulltext', 'category_id = ' . $category->getId() . ' AND store_id = ' . $category->getStoreId());        
+        if ($category->getIsActive() && $category->getName()!='') {
+            $data = array(
+                'category_id' => $category->getId(),
+                'data_index' => $category->getName(),
+                'store_id' => $category->getStoreId()
+            );                        
+
+            $connection->insert($tablePrefix . 'catalogsearch_category_fulltext', $data);
+        }    
+    }
+    
+    public function removeCategoryFromSearchIndex($observer) {        
+        $category = $observer->getEvent()->getCategory();
+        if (!$category || !$category->getId()) return false;
+        
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $tablePrefix = (string) Mage::getConfig()->getTablePrefix();
+
+        $connection->delete($tablePrefix . 'catalogsearch_category_fulltext', 'category_id = ' . $category->getId() . ' AND store_id = ' . $category->getStoreId());                
+    }
+    
+
+    public function regenerateStoreIndex($observer) {
+        $store = $observer->getEvent()->getStore();        
+        if (!isset($store)) {
+            $store = $observer->getEvent()->getObject();
+        }
+        
+        if (isset($store)) {
+            $storeId = $store->getId();
+            Mage::getModel('searchautocomplete/fulltext')->regenerateIndex($storeId);
+        }
+        
+        return $this;
+    }
+
+    public function cleanCmsPageIndex($observer) {
+        $page = $observer->getEvent()->getObject();
+
+        Mage::getModel('searchautocomplete/fulltext')
+                ->cleanIndex(null, $page->getId())
+                ->resetSearchResults();
+
+        return $this;
+    }
+
+    public function regenerateFullIndex($observer) {
+        Mage::getModel('searchautocomplete/fulltext')->regenerateIndex();        
+        return $this;
+    }
+
+}
---
+++ app/code/local/MageWorx/SearchAutocomplete/controllers/AjaxController.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * MageWorx
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MageWorx EULA that is bundled with
+ * this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.mageworx.com/LICENSE-1.0.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the extension
+ * to newer versions in the future. If you wish to customize the extension
+ * for your needs please refer to http://www.mageworx.com/ for more information
+ *
+ * @category   MageWorx
+ * @package    MageWorx_SearchAutocomplete
+ * @copyright  Copyright (c) 2011 MageWorx (http://www.mageworx.com/)
+ * @license    http://www.mageworx.com/LICENSE-1.0.html
+ */
+/**
+ * Search Autocomplete extension
+ *
+ * @category   MageWorx
+ * @package    MageWorx_SearchAutocomplete
+ * @author     MageWorx Dev Team
+ */
+
+require_once 'Mage/CatalogSearch/controllers/AjaxController.php';
+
+class MageWorx_SearchAutocomplete_AjaxController extends Mage_CatalogSearch_AjaxController {
+
+    public function suggestAction() {        
+        if (!Mage::helper('searchautocomplete')->isEnabled()) return parent::suggestAction();        
+                
+        //add query filter prior
+        
+        $queryText = $this->getRequest()->getParam('q', false);
+        
+		if (!$queryText || $queryText == '') {
+            exit();
+        }
+        $words = Mage::helper('core/string')->splitWords($queryText, true, 0);
+        $deniedWords = Mage::getStoreConfig('mageworx_tweaks/searchautocomplete/exlude_words_in_search',Mage::app()->getStore());
+        
+        $listDeniedWords = Mage::helper('core/string')->splitWords( $deniedWords, true, 0,",");
+        $queryText = "";
+        foreach ( $words as $word ) {            
+            if( !in_array( $word, $listDeniedWords )){
+                $queryText .= $word." ";                
+            }                        
+        }
+		
+        $queryModel = Mage::helper('catalogsearch')->getQuery();
+        $queryModel->setStoreId(Mage::app()->getStore()->getId());
+        $queryModel->prepare();
+        $queryModel->save();
+
+        
+        if (Mage::helper('searchautocomplete')->isSuggestedSearches()) {
+            //$suggestData = $this->getLayout()->createBlock('catalogsearch/autocomplete')->getSuggestData();
+            $suggestData = Mage::getResourceModel('searchautocomplete/query_collection')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->setQueryFilter($queryText)
+                ->setLimit(Mage::helper('searchautocomplete')->getSuggestedSearchesResults())
+                ->toArray();
+				
+        } else {
+            $suggestData = array();
+        }    
+        
+        if (Mage::helper('searchautocomplete')->isSearchProducts()) {
+            $products = Mage::getModel('catalogsearch/layer')->getProductCollection()->load();
+        } else {
+            $products = array();
+        }
+		
+        if (Mage::helper('searchautocomplete')->isSearchCategories()) {
+            $categories = Mage::getModel('searchautocomplete/search')->getRelevantCategoriesByQuery();
+        } else {
+            $categories = array();
+        }    
+        
+        if (Mage::helper('searchautocomplete')->isSearchCms()) {
+            $cmsPages = Mage::getModel('searchautocomplete/search')->getCmsPageCollection();
+        } else {
+            $cmsPages = array();
+        }
+
+        if (count($suggestData) && count($products) && count($categories) && count($cmsPages)) {
+            $this->getResponse()->setBody($this->getLayout()->createBlock('searchautocomplete/autocomplete')
+                    ->setSuggestData($suggestData)
+                    ->setProducts($products)
+                    ->setCategories($categories)
+                    ->setCmsPages($cmsPages)                    
+                    ->toHtml());
+        } elseif ( !count($suggestData) && count($products) && count($categories) && count($cmsPages) ) {
+            $this->getResponse()->setBody($this->getLayout()->createBlock('searchautocomplete/autocomplete')                    
+                    ->setProducts($products)
+                    ->setCategories($categories)
+                    ->setCmsPages($cmsPages)                    
+                    ->toHtml());         
+        } elseif ( !count($suggestData) && count($products) && count($categories) && !count($cmsPages) ) {
+            $this->getResponse()->setBody($this->getLayout()->createBlock('searchautocomplete/autocomplete')                    
+                    ->setProducts($products)
+                    ->setCategories($categories)
+                    ->toHtml());         
+        } elseif ( !count($suggestData) && count($products) && count($categories) && !count($cmsPages) ) {
+            $this->getResponse()->setBody($this->getLayout()->createBlock('searchautocomplete/autocomplete')                    
+                    ->setProducts($products)
+                    ->setCategories($categories)
+                    ->toHtml());         
+        } elseif ( count($products) && !count($categories)  ) {
+            $this->getResponse()->setBody($this->getLayout()->createBlock('searchautocomplete/autocomplete')
+                    ->setProducts($products)                    
+                    ->toHtml());
+        } elseif ( !count($products) && count($categories)  ) {
+            $this->getResponse()->setBody($this->getLayout()->createBlock('searchautocomplete/autocomplete')
+                    ->setCategories($categories)                                    
+                    ->toHtml());         
+        } else {
+            exit();
+        }
+    }
+}
